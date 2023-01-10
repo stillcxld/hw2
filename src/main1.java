5d3c91cf74ab5ad1af77afd04be9e8950c3dcd16
stillcxld <119540967+stillcxld@users.noreply.github.com>
2023-01-10 23:35:12 +0400

    Update main1.java

--- src/main1.java
+++ src/main1.java
@@ -1,11 +1,11 @@
- public class main1
-{ public static void main(String[] args) {
+public class main1 {
+    public static void main(String[] args) {
 
-    int ticket = 400; // Стоимость билета
-    int bonus = 25;   // Кол-во рублей для одной бонусной мили
-    int miles = (ticket/bonus);
+        int ticket = 400; // Стоимость билета
+        int bonus = 25;   // Кол-во рублей для одной бонусной мили
+        int miles = (ticket / bonus);
 
-    System.out.println("Начислено бонусных миль:");
-    System.out.println(miles);
-}
+        System.out.println("Начислено бонусных миль:");
+        System.out.println(miles);
+    }
 }
